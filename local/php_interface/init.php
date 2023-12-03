e012b9f2b46b0ef709062f02b44300e8ae82183e
Evgeny <evgeny.legek@yandex.ru>
2023-12-03 16:18:30 +0300

    Start project

---
+++ local/php_interface/init.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Вырезаем type="text/javascript ==============================================================================
+AddEventHandler("main", "OnEndBufferContent", "removeType");
+function removeType(&$content)
+{
+    $content = replace_output($content);
+}
+function replace_output($d)
+{
+    return str_replace(' type="text/javascript"', "", $d);
+}
